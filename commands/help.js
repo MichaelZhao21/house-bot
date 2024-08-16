e24f453847280007cad0200dcd8c0307b02478cf
Michael Zhao <michaelzhao314@gmail.com>
2024-08-16 01:28:48 -0500

    Update help to include all commands

--- commands/help.js
+++ commands/help.js
@@ -24,7 +24,27 @@ module.exports = {
             },
             {
                 name: "/people",
-                value: "Shows the list of people in the house",
+                value: "Shows the list of people in the house and their info",
+            },
+            {
+                name: "/rent",
+                value: "Shows the rent that you still owe for the month",
+            },
+            {
+                name: "/chores",
+                value: "Shows the chores you have for the week",
+            },
+            {
+                name: "/finish",
+                value: "Sets a specific chore to 'done', can also set a chore to not done",
+            },
+            {
+                name: "/trash",
+                value: "Notifies the next person to take out the trash, will rotate",
+            },
+            {
+                name: "/recycling",
+                value: "Notifies the next person to take out the recycling, will rotate",
             },
             {
                 name: "/events",
@@ -39,12 +59,13 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor(0xe0b5f5)
             .setTitle("Commands for House Bot!")
-            .setDescription("These are all the commands that can be used. Admin commands are not listed and prefixed with 'admin'.")
+            .setDescription(
+                "These are all the commands that can be used. Admin commands are not listed and prefixed with 'admin'."
+            )
             .setThumbnail(
                 "https://as1.ftcdn.net/v2/jpg/05/74/72/86/1000_F_574728684_voyDBkCWq2cKEd3MDoNE0nQLrWYFVxFh.jpg"
             )
-            .addFields(commands)
-            .setTimestamp();
+            .addFields(commands);
 
         interaction.reply({ embeds: [embed] });
     },
